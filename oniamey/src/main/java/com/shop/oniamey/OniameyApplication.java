6421033235bf025bd33cb0b2c63490d29aec3acc
nmh <minhhieu8723@gmail.com>
2023-09-25 23:30:57 +0700

    upload base project

---
+++ oniamey/src/main/java/com/shop/oniamey/OniameyApplication.java
@@ -0,0 +1,13 @@
+package com.shop.oniamey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OniameyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OniameyApplication.class, args);
+    }
+
+}
---
+++ oniamey/src/test/java/com/shop/oniamey/OniameyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.oniamey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OniameyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
