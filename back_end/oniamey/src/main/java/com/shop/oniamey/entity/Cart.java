9c0e3a276322d5a30ba09cd54bae95e3aa666f1a
Oniamey <nyp2k4@gmail.com>
2023-09-26 11:30:28 +0700

    push entity Cart - CartDetail - Category - Image - Size

---
+++ back_end/oniamey/src/main/java/com/shop/oniamey/entity/Cart.java
@@ -0,0 +1,37 @@
+package com.shop.oniamey.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+@Entity(name = "cart")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cart", nullable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "create_at", nullable = false)
+    private Date createAt;
+//
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "", nullable = false)
+}
