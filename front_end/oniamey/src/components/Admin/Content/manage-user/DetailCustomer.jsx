ef8d4422e573365425623cd613b79e1558822c4a
nmh <minhhieu8723@gmail.com>
2023-10-15 02:40:35 +0700

    build address function for customer

--- front_end/oniamey/src/components/Admin/Content/manage-user/DetailCustomer.jsx
+++ front_end/oniamey/src/components/Admin/Content/manage-user/DetailCustomer.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Container, Form, Image } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Button, Container, Form, Image } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Tabs, Tab } from "react-bootstrap";
 import Upload from "rc-upload";
+import { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import DataTable from "react-data-table-component";
 
+import service from "../../../../services/CustomerService";
 import "../manage-user/style/DetailCustomer.css";
 import defaultImage from "../manage-user/style/image/man.png";
+import ModifyAddressModal from "./components/ModifyAddressModal";
+import "../manage-user/style/Table.css";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faPenSquare, faSquare } from "@fortawesome/free-solid-svg-icons";
 
 const DetaiCustomer = () => {
   const [fullName, setFullName] = useState("");
@@ -18,13 +26,27 @@ const DetaiCustomer = () => {
   const [gender, setGender] = useState();
   const [avatar, setAvatar] = useState("");
   const { id } = useParams();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [addresses, setAddresses] = useState([]);
+
+  const openModal = async (idCustomer) => {
+    if (id) {
+      setIsModalOpen(true);
+    } else {
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    getAddresses();
+  };
 
   const props = {
     action: "http://localhost:8088/api/storage/uploadFile",
     multiple: true,
     onStart(file) {
       console.log("onStart", file, file.name);
-
     },
     onSuccess(ret) {
       console.log("onSuccess", ret);
@@ -32,7 +54,6 @@ const DetaiCustomer = () => {
     },
     onError(err) {
       console.log("onError", err);
-
     },
     beforeUpload(file, fileList) {
       console.log(file, fileList);
@@ -46,85 +67,242 @@ const DetaiCustomer = () => {
     },
   };
 
+  const handleSubmit = () => {
+    const customer = {
+      fullName,
+      birthDate,
+      email,
+      phoneNumber,
+      gender,
+      avatar,
+      isDeleted,
+    };
+
+    service
+      .updateCustomer(customer, id)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getAddresses = async () => {
+    await service
+      .getAddressesById(id)
+      .then((res) => {
+        console.log(res.data);
+        setAddresses(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleChangeDefaultAddress = (id) => {
+    return () => {
+      service
+        .changeDefaultAddress(id)
+        .then((res) => {
+          console.log(res.data);
+          getAddresses();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  };
+
+  const columns = [
+    {
+      name: "Line",
+      center: true,
+      selector: (row) => row.line,
+    },
+    {
+      name: "Province",
+      center: true,
+      selector: (row) => row.province,
+    },
+    {
+      name: "District",
+      center: true,
+      selector: (row) => row.district,
+    },
+    {
+      name: "Ward",
+      center: true,
+      selector: (row) => row.ward,
+    },
+    {
+      name: "Action",
+      center: true,
+      cell: (row) => (
+        <>
+          <Button
+            className="btn btn-dark"
+            onClick={() => {}}
+            style={{ marginLeft: "10px" }}
+          >
+            <FontAwesomeIcon icon={faPenSquare} />
+          </Button>
+          {row.isDefault ? (
+            <Button
+              className="btn btn-dark"
+              // onClick={() => {}}
+              style={{ marginLeft: "10px" }}
+            >
+              <FontAwesomeIcon icon={faCheck} />
+            </Button>
+          ) : (
+            <Button
+              className="btn btn-dark"
+              onClick={handleChangeDefaultAddress(row.id)}
+              style={{ marginLeft: "10px" }}
+            >
+              <FontAwesomeIcon icon={faSquare} />
+            </Button>
+          )}
+        </>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    console.log(id);
+    service
+      .getCustomerById(id)
+      .then((res) => {
+        console.log(res.data);
+        setFullName(res.data.fullName);
+        setEmail(res.data.email);
+        setIsDeleted(res.data.isDeleted);
+        setPhoneNumber(res.data.phoneNumber);
+        setBirthDate(res.data.birthDate);
+        setAvatar(res.data.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    getAddresses();
+  }, []);
+
   return (
-    <Container className="detail-form">
-      <h4>Detail Customer</h4>
-      <Tabs
-        defaultActiveKey="home"
-        id="uncontrolled-tab-example"
-        className="mb-3"
-      >
-        <Tab eventKey="home" title="About">
-          <h5>Information</h5>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicAvatar">
-              <Upload {...props}>
-                <Image src={avatar ? avatar : defaultImage} roundedCircle alt="avatar" style={{ maxWidth: "100px" }} />
-              </Upload>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicFullname">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter full name"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter phone number"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicDateOfBirth">
-              <Form.Label>Date of Birth</Form.Label>
-              <Form.Control
-                type="date"
-                value={birthDate}
-                onChange={(e) => setBirthDate(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Gender</Form.Label>
-              <Form.Select
-                value={gender}
-                onChange={(e) => setGender(e.target.value)}
-              >
-                <option value={1}>Male</option>
-                <option value={2}>Female</option>
-                <option value={3}>Another</option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Status</Form.Label>
-              <Form.Select
-                value={isDeleted}
-                onChange={(e) => setIsDeleted(e.target.value)}
-              >
-                <option value={true}>Active</option>
-                <option value={false}>Deactivate</option>
-              </Form.Select>
-            </Form.Group>
-          </Form>
-        </Tab>
-        <Tab eventKey="profile" title="Address">
-          <h5>Address Notebook</h5>
-        </Tab>
-      </Tabs>
-    </Container>
+    <Fragment>
+      <h4 className="detail-customer-title">Detail Customer</h4>
+      <Container className="detail-form h-75">
+        <Tabs
+          defaultActiveKey="home"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="About">
+            <div className="d-flex justify-content-between align-items-center">
+              <h5>Information</h5>
+              <div className="">
+                <Link
+                  to="/admins/manage-customers"
+                  className="btn btn-secondary"
+                >
+                  Back
+                </Link>
+                <Button
+                  variant="dark"
+                  onClick={handleSubmit}
+                  style={{ marginLeft: "3px" }}
+                >
+                  Save Changes
+                </Button>
+              </div>
+            </div>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicAvatar">
+                <Upload {...props}>
+                  <Image
+                    src={avatar ? avatar : defaultImage}
+                    roundedCircle
+                    alt="avatar"
+                    style={{ maxWidth: "100px" }}
+                  />
+                </Upload>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicFullname">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter full name"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter phone number"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicDateOfBirth">
+                <Form.Label>Date of Birth</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={birthDate}
+                  onChange={(e) => setBirthDate(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Gender</Form.Label>
+                <Form.Select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value={1}>Male</option>
+                  <option value={2}>Female</option>
+                  <option value={3}>Another</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Status</Form.Label>
+                <Form.Select
+                  value={isDeleted}
+                  onChange={(e) => setIsDeleted(e.target.value)}
+                >
+                  <option value={true}>Active</option>
+                  <option value={false}>Deactivate</option>
+                </Form.Select>
+              </Form.Group>
+            </Form>
+          </Tab>
+          <Tab eventKey="profile" title="Address">
+            <h5>Address Notebook</h5>
+            <div className="d-flex justify-content-end mb-2">
+              <Button variant="dark" onClick={() => openModal(id)}>
+                Add Address
+              </Button>
+            </div>
+            <div className="table-container p-0">
+              <DataTable columns={columns} data={addresses} />
+            </div>
+          </Tab>
+        </Tabs>
+        <ModifyAddressModal isOpen={isModalOpen} onClose={closeModal} id={id} />
+      </Container>
+    </Fragment>
   );
 };
 
--- front_end/oniamey/src/components/Admin/Content/manage-user/components/ModifyAddressModal.jsx
+++ front_end/oniamey/src/components/Admin/Content/manage-user/components/ModifyAddressModal.jsx
@@ -1,20 +1,215 @@
 import React from "react";
-import { Form, FloatingLabel } from "react-bootstrap";
+import { Form, FloatingLabel, Modal, Button, Col, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-const ModifyAddressModal = () => {
+import provinceService from "../../../../../services/ProvinceService";
+import CustomerService from "../../../../../services/CustomerService";
+
+const ModifyAddressModal = ({ isOpen, onClose, id }) => {
+  const [address, setAddress] = useState({
+    province: "",
+    district: "",
+    ward: "",
+    line: "",
+    customerId: id,
+    isDefault: false,
+    isDeleted: false,
+  });
+  const [provinces, setProvinces] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [wards, setWards] = useState([]);
+  const [provinceId, setProvinceId] = useState();
+  const [districtId, setDistrictId] = useState();
+
+  useEffect(() => {
+    if (id > 0) {
+      provinceService
+        .getProvinces()
+        .then((response) => {
+          console.log(response.data);
+          setProvinces(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    return () => {
+      setProvinces([]);
+    };
+  }, [id]);
+
+  useEffect(() => {
+    if (address.province) {
+      provinceService
+        .getDistricts(provinceId)
+        .then((response) => {
+          console.log(response.data.districts);
+          setDistricts(response.data.districts);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    return () => {
+      setDistricts([]);
+      setWards([]);
+    };
+  }, [provinceId]);
+
+  useEffect(() => {
+    if (address.district) {
+      provinceService
+        .getWards(districtId)
+        .then((response) => {
+          console.log(response.data.wards);
+          setWards(response.data.wards);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    return () => {
+      setWards([]);
+    };
+  }, [districtId]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAddress({ ...address, [name]: value });
+    if (name === "province") {
+      const selectedProvince = provinces.find(
+        (province) => province.name === value
+      );
+      setProvinceId(selectedProvince ? selectedProvince.code : null);
+    }
+
+    if (name === "district") {
+      const selectedDistrict = districts.find(
+        (district) => district.name === value
+      );
+      setDistrictId(selectedDistrict ? selectedDistrict.code : null);
+    }
+
+    return () => {
+      setAddress({
+        province: "",
+        district: "",
+        ward: "",
+        line: "",
+        customerId: id,
+        isDefault: false,
+        isDeleted: false,
+      });
+    };
+  };
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleSubmit = () => {
+    console.log(address);
+    CustomerService.createAddress(address)
+      .then((response) => {
+        console.log(response.data);
+        onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-      <FloatingLabel
-          controlId="floatingSelectGrid"
-          label="Works with selects"
-        >
-          <Form.Select aria-label="Floating label select example">
-            <option>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </Form.Select>
-        </FloatingLabel>
+      <Modal show={isOpen} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{id > 0 ? "Update Address" : "Add Address"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="g-2">
+            <Col md>
+              <FloatingLabel
+                controlId="floatingSelect"
+                label="Choose Province / City"
+                className="mb-3"
+              >
+                <Form.Select
+                  aria-label="Floating label select example"
+                  name="province"
+                  onChange={(e) => handleChange(e)}
+                  value={address.province}
+                >
+                  <option>--Choose One--</option>
+                  {provinces.map((province) => (
+                    <option key={province.code} value={province.name}>
+                      {province.name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </FloatingLabel>
+              <FloatingLabel controlId="floatingSelect" label="Choose Ward">
+                <Form.Select
+                  aria-label="Floating label select example"
+                  name="ward"
+                  onChange={handleChange}
+                  value={address.ward}
+                >
+                  <option>--Choose One--</option>
+                  {wards.map((ward) => (
+                    <option key={ward.code} value={ward.name}>
+                      {ward.name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+            <Col md>
+              <FloatingLabel
+                controlId="floatingSelectGrid"
+                label="Choose District"
+                className="mb-3"
+              >
+                <Form.Select
+                  aria-label="Floating label select example"
+                  name="district"
+                  onChange={handleChange}
+                  value={address.district}
+                >
+                  <option>--Choose One--</option>
+                  {districts.map((district) => (
+                    <option key={district.code} value={district.name}>
+                      {district.name}
+                    </option>
+                  ))}
+                </Form.Select>
+              </FloatingLabel>
+              <FloatingLabel
+                controlId="floatingInputGrid"
+                label="Address Detail"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Address Detail"
+                  name="line"
+                  onChange={handleChange}
+                  value={address.line}
+                />
+              </FloatingLabel>
+            </Col>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="dark" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
