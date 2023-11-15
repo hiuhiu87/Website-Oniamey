d92e8bbc05338269c27529670ac3249dcd7aa7b5
lucluat <lucvanluat@gmail.com>
2023-11-15 14:42:45 +0700

    code api service

--- front_end/oniamey/src/services/OrderDetailApi.js
+++ front_end/oniamey/src/services/OrderDetailApi.js
@@ -3,6 +3,15 @@ import instance from '../utils/axiosCustomize';
 
 const urlOrderDetail = "api/admin/order-detail";
 
+export  const createOrderDetail = async (data)=>{
+    try {
+        const res= await instance.post(urlOrderDetail,data);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
 export const getListProductByOrderId =async (idOrder) => {
     try {
         const result =await instance.get(`${urlOrderDetail}/${idOrder}`);
